88f14c3b46cd2561afedfe2593f985c1b8c2c351
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-08 08:21:26 +0900

    fix: favicon.ico 접속 시 에러 처리

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
     res.send("My first express app");
 });
 
+router.get("/favicon.ico", function (req, res) {
+    res.sendStatus(204);
+});
+
 module.exports = router;
