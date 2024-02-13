ff10d718cf9d9d9f5a4503ed7fb91323a6f97268
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-13 11:17:08 +0900

    feat: 로그인 된 유저만 댓글 삭제

--- routes/comment.js
+++ routes/comment.js
@@ -70,20 +70,36 @@ router.put("/:id", function (req, res, next) {
 
 // 삭제
 router.delete("/:id", function (req, res, next) {
-    const commentId = req.params.id;
+    try {
+        const authToken = req.cookies.authToken;
+        if (authToken) {
+            const decodedToken = verifyToken(authToken);
+            const userId = decodedToken._id;
+            const commentId = req.params.id;
 
-    console.log("delete", req.params.id);
-    Comment.findByIdAndDelete(commentId)
-        .then((data) => {
-            if (!data)
-                return res
-                    .status(404)
-                    .json({ message: "댓글을 찾을 수 없습니다." });
-            res.json(data);
-        })
-        .catch((err) => {
-            return next(err);
-        });
+            Comment.findById(commentId).then((data) => {
+                if (!data)
+                    return res
+                        .status(404)
+                        .json({ message: "댓글을 찾을 수 없습니다." });
+
+                if (data.author != userId) {
+                    return res
+                        .status(401)
+                        .send("Bad Request: 다른 유저입니다.");
+                }
+
+                data.deleteOne().then((data) => {
+                    console.log("delete", commentId);
+                    res.json(data);
+                });
+            });
+        } else {
+            res.status(401).send("Bad Request: 로그인이 필요합니다.");
+        }
+    } catch (err) {
+        return next(err);
+    }
 });
 
 module.exports = router;
