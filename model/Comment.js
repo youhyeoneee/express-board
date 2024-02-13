96e71578742c21caf5e3895cd5d479233513f746
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-13 10:50:34 +0900

    feat: Comment 스키마에 boardId(board 참조), author(user 참조) 하도록 변경, updatedAt 추가

--- model/Comment.js
+++ model/Comment.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-    boardId: { type: String, required: true },
     content: { type: String, required: true },
-    author: String,
+    boardId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Board",
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
     createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
